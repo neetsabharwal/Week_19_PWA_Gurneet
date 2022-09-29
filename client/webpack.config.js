938a2ef87eaa6748b383a9322a7ac7c5fdafc515
Gurneet <gsingh2@atb.com>
2022-09-29 13:26:08 -0400

    service worker and pwa

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,6 @@ const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
 
-
 module.exports = () => {
   return {
     mode: "production",
@@ -19,6 +18,10 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'Webpack Plugin',
+      }),
+      new InjectManifest({
+        swSrc: './src/src-sw.js',
+        swDest: 'src-sw.js',
       }),
       new WebpackPwaManifest({
         name: 'JATE',
